80f98d118bd07b824fa736edcb399744cdeb7d34
muhammadjuniar <muhammadjuniar77@gmail.com>
2024-07-30 17:22:47 +0700

    update: member area

--- src/views/MemberAreaView.vue
+++ src/views/MemberAreaView.vue
@@ -1,15 +1,32 @@
 <template>
-    <div class="row justify-content-center m0">
-        <div class="col-12 col-lg-3 content-body">
-            <div class="row">
-                Member Area
-            </div>
+    <div>
+        <div v-for="(tax, index) in taxes" :key="index">
+            <input v-model="taxes[index]" />
+            <button type="number" @click="remove(index)">Remove</button>
         </div>
+        <button type="number" @click="add">Add</button>
+        <p>Count: {{ taxes.length }}</p>
     </div>
 </template>
 
-<script>
+<script>0
 export default {
-  name: 'MemberArea',
-}
+    name: 'MemberArea',
+    data() {
+        return {
+            taxes: [0]
+        }
+    },
+    methods: {
+        add() {
+            this.taxes.push(0);
+        },
+
+        remove(index) {
+            if (this.taxes.length > 1) {
+                this.taxes.splice(index, 1);
+            }
+        }
+    }
+};
 </script>
